67f144a683a9e46bd8dcc3bcbd3db47e794a7f93
Derek VerLee <dverlee@resolutebi.com>
2018-01-15 14:10:37 -0500

    Line transposition convenience

--- init.el
+++ init.el
@@ -164,6 +164,29 @@
 
 
 
+;; transpose lines convenience functions from
+;;    http://emacsredux.com/blog/2013/04/02/move-current-line-up-or-down/
+
+(defun move-line-up ()
+  "Move up the current line."
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2)
+  (indent-according-to-mode))
+
+(defun move-line-down ()
+  "Move down the current line."
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+(global-set-key (kbd "M-S-<down>")  'move-line-down)
+(global-set-key (kbd "M-S-<up>")  'move-line-up)
+
+
+
 ;; -- custom set variables stuff --
 ;;; this just magically appears, I guess
 
