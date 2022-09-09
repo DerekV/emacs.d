0495493ca9015bb5073fa41424f417b0067d2fd9
Derek VerLee <derekverlee@gmail.com>
2022-09-09 11:24:10 -0400

    Add backup file tweaks

--- init.el
+++ init.el
@@ -251,3 +251,22 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(defconst emacs-paranoid-dir (concat user-emacs-directory "backups"))
+
+(setq backup-directory-alist
+      `(("." . ,emacs-paranoid-dir)))
+(setq auto-save-file-name-transforms
+      `((".*" ,emacs-paranoid-dir)))
+
+(setq make-backup-files t               ; backup of a file the first time it is saved.
+      backup-by-copying-when-linked t               ; don't clobber symlinks
+      version-control t                 ; version numbers for backup files
+      delete-old-versions t             ; delete excess backup files silently
+      delete-by-moving-to-trash t
+      kept-old-versions 6               ; oldest versions to keep when a new numbered backup is made (default: 2)
+      kept-new-versions 9               ; newest versions to keep when a new numbered backup is made (default: 2)
+      auto-save-default t               ; auto-save every buffer that visits a file
+      auto-save-timeout 30              ; number of seconds idle time before auto-save (default: 30)
+      auto-save-interval 300            ; number of keystrokes between auto-saves (default: 300)
+      )
